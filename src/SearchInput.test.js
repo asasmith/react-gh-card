ced87ddae7905f8d3441d7f1fbf3ae74fab6ea31
Asa Smith <smith.asa.la@gmail.com>
2019-02-03 11:16:12 -0500

    add test for SearchInput component
    - state.inputVal updates on change event

--- src/SearchInput.test.js
+++ src/SearchInput.test.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { render, getByTestId } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 import SearchInput from './SearchInput'
 
-test('<SearchInput />', () => {
-  const { debug, getByTestId } = render(<SearchInput />)
-  const searchInput = getByTestId('search-input')
+afterEach(cleanup)
 
-  debug()
+const inputChange = jest.fn()
+
+test('<SearchInput />', () => {
+  const { debug, getByTestId } = render(<SearchInput inputChange={inputChange} />)
+  const searchInput = getByTestId('search-input')
 
   expect(searchInput.tagName).toBe('INPUT')
   expect(searchInput.placeholder).toBe('Search GH User')
+  fireEvent.change(searchInput, {target: {value: 'testing'}})
+  expect(searchInput.value).toBe('testing')
+  expect(inputChange).toHaveBeenCalledTimes(1)
 })
