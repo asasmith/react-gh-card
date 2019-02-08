c4267f5428619aff15aa177e4f0a8e127d87d289
Asa Smith <smith.asa.la@gmail.com>
2019-02-08 09:54:33 -0500

    test Title component props

--- src/Title.test.js
+++ src/Title.test.js
@@ -1,27 +1,39 @@
 import React from 'react'
-import { render, cleanup, waitForElement } from 'react-testing-library'
+import { render, cleanup, waitForElement, getByTestId } from 'react-testing-library'
 import Title from './Title'
 
-global.fetch = require('jest-fetch-mock')
 
-afterEach(() => {
-  cleanup()
-  console.error.mockClear() // what is this?
-})
 
-console.error = jest.fn()
+test('<Title /> ', () => {
 
-test('<Title /> ', async () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({
-      user: {
-        name: 'testing mockResponse',
-      }
-    })
-  )
+  const user = {
+    title: 'some title',
+    url: 'http://something.com',
+    location: 'where ever',
+    bio: 'a short bio here',
+    repos: '10',
+    followers: '10',
+    following: '10',
+  }
 
-  const { debug, getByText } = render(<Title title={name}/>)
+  const { getByTestId } = render(<Title {...user}/>)
 
-  await waitForElement(() => getByText('testing mockResponse'))
-  debug()
+  expect(getByTestId('name').tagName).toBe('H1')
+  expect(getByTestId('name').textContent).toBe(user.title)
+  
+  expect(getByTestId('github-url').tagName).toBe('A')
+  expect(getByTestId('github-url')).toHaveAttribute('HREF', user.url)
+  
+  expect(getByTestId('bio').tagName).toBe('P')
+  expect(getByTestId('bio').textContent).toBe(user.bio)
+
+  expect(getByTestId('repos').tagName).toBe('P')
+  expect(getByTestId('repos').textContent).toBe(`Repos: ${user.repos}`)
+
+  expect(getByTestId('followers').tagName).toBe('P')
+  expect(getByTestId('followers').textContent).toBe(`Followers: ${user.followers}`)
+  
+  expect(getByTestId('following').tagName).toBe('P')
+  expect(getByTestId('following').textContent).toBe(`Following: ${user.following}`)
+
 })
